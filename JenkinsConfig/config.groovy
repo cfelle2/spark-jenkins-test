d1ff1a016546aa49c35263d19b54f2927e033500
Christopher Feller <Christopher.Feller@nielsen.com>
2018-10-25 11:26:53 -0500

    test

--- JenkinsConfig/config.groovy
+++ JenkinsConfig/config.groovy
@@ -1,10 +1,7 @@
 import groovy.transform.Field
 
-//@Field private BucketName = "useast1-nlsn-mediaanalytics-emr"
-private BucketName = "useast1-nlsn-mediaanalytics-emr"
-//@Field private AppPath = "script/jenkinstest"
-private AppPath = "script/jenkinstest"
-
+@Field private BucketName = "useast1-nlsn-mediaanalytics-emr"
+@Field private AppPath = "script/jenkinstest"
 
 def init(bucketname, apppath) {
     BucketName = bucketname
