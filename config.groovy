0e73897711eeebcad035ad4f94d0b05c85a94213
Christopher Feller <Christopher.Feller@nielsen.com>
2018-10-25 09:34:04 -0500

    test

--- config.groovy
+++ config.groovy
@@ -1,15 +1,16 @@
 import groovy.transform.Field
-@Field private BucketName = "useast1-nlsn-mediaanalytics-emr"
-@Field private AppPath = "script/jenkinstest"
+@Field private BucketName = null
+//@Field private AppPath = "script/jenkinstest"
 
 def init(bucketname, apppath) {
     BucketName = bucketname
-    AppPath = apppath
+  //  AppPath = apppath
 }
 def getBucketName(){
-    return BucketName
+    //return BucketName
+    return "useast1-nlsn-mediaanalytics-emr"
 }
-def getAppPath(){
-    return AppPath
-}
+//def getAppPath(){
+//    return AppPath
+//}
 return this
