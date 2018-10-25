b9dc2f8c948631af1bd0a9c0ed09af678c799898
Christopher Feller <Christopher.Feller@nielsen.com>
2018-10-25 09:29:59 -0500

    test

--- config.groovy
+++ config.groovy
@@ -4,7 +4,7 @@ import groovy.transform.Field
 /*
 def bucket_name="useast1-nlsn-mediaanalytics-emr"
 def app_path="script/jenkinstest"
-
+hello
 def delete_path= app_path + "/"
 def upload_path= bucket_name + "/" + app_path
 /*
